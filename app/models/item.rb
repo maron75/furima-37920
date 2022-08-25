8d17bf3f51e79abad6694c1a2bf4a19f09030a0f
maron75 <hirokana.827562113@gmail.com>
2022-08-25 15:34:08 +0900

    バリデーション設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,12 @@
 class Item < ApplicationRecord
+
+  validates :name, presence: true
+  validates :detail, presence: true
+  validates :category_id, presence: true
+  validates :condition_id, presence: true
+  validates :shipping_cost_id, presence: true
+  validates :prefecture_id, presence: true
+  validates :shipping_day_id, presence: true
+  validates :price, presence: true
+  validates :user, presence: true
 end
