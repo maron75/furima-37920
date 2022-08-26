5e42311fe7fe2bfcd8c2d0515c6de347ca58b9aa
maron75 <hirokana.827562113@gmail.com>
2022-08-26 15:53:26 +0900

    ActiveHash導入2

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,11 +2,19 @@ class Item < ApplicationRecord
 
   validates :name, presence: true
   validates :detail, presence: true
-  validates :category_id, presence: true
-  validates :condition_id, presence: true
-  validates :shipping_cost_id, presence: true
-  validates :prefecture_id, presence: true
-  validates :shipping_day_id, presence: true
+  validates :category_id, presence: true, numericality: { other_than: 1 , message: "can't be blank"} 
+  validates :condition_id, presence: true, numericality: { other_than: 1 , message: "can't be blank"} 
+  validates :shipping_cost_id, presence: true, numericality: { other_than: 1, message: "can't be blank"} 
+  validates :prefecture_id, presence: true, numericality: { other_than: 1 , message: "can't be blank"} 
+  validates :shipping_day_id, presence: true, numericality: { other_than: 1 , message: "can't be blank"} 
   validates :price, presence: true
   validates :user, presence: true
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :condition
+  belongs_to :shipping_cost
+  belongs_to :prefecture
+  belongs_to :shipping_day
 end
+
