1b3009b05b800feaf909b808fec28851356082df
maron75 <hirokana.827562113@gmail.com>
2022-08-29 14:42:11 +0900

    修正1

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,15 +1,15 @@
 class Item < ApplicationRecord
 
   has_one_attached :image
-
+  validates :image, presence: true
   validates :name, presence: true
   validates :detail, presence: true
   validates :category_id, presence: true, numericality: { other_than: 1 , message: "can't be blank"} 
   validates :condition_id, presence: true, numericality: { other_than: 1 , message: "can't be blank"} 
   validates :shipping_cost_id, presence: true, numericality: { other_than: 1, message: "can't be blank"} 
-  validates :prefecture_id, presence: true, numericality: { other_than: 1 , message: "can't be blank"} 
+  validates :prefecture_id, presence: true, numericality: { other_than: 0 , message: "can't be blank"} 
   validates :shipping_day_id, presence: true, numericality: { other_than: 1 , message: "can't be blank"} 
-  validates :price, presence: true, format: { with: /\A[0-9]+\z/, message: "Half-width number" }, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999, message: "Out of setting range" }
+  validates :price, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }
   
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :category
--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -21,6 +21,12 @@ RSpec.describe Item, type: :model do
 
 
     context '出品ができない時' do
+      it 'ユーザーは登録している人であること' do
+        @item.user_id = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include('User must exist', "User can't be blank")
+      end
+      
       it '商品画像を1枚つけることが必須であること' do
         @item.image.key = ''
         @item.valid?
@@ -41,6 +47,36 @@ RSpec.describe Item, type: :model do
 
       it 'カテゴリーの情報が必須であること' do
         @item.category_id = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Category Select")
+      end
+
+      it 'カテゴリーに「---」が選択されている場合は出品できない' do
+        @item.category_id = 1
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Category Select")
+      end
+
+      it '商品の状態が「---」以外であれば登録できる' do
+        @item.condition_id = 1
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Category Select")
+      end
+
+      it '配送料の負担が「---」以外であれば登録できる' do
+        @item.shipping_cost_id = 1
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Category Select")
+      end
+
+      it '発送元の地域が「---」以外であれば登録できる' do
+        @item.prefecture_id = 0
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Category Select")
+      end
+
+      it '発送までの日数が「---」以外であれば登録できる' do
+        @item.shipping_day_id = 1
         @item.valid?
         expect(@item.errors.full_messages).to include("Category Select")
       end
@@ -72,13 +108,19 @@ RSpec.describe Item, type: :model do
       it '販売価格についての情報が必須であること' do
         @item.price = ''
         @item.valid?
-        expect(@item.errors.full_messages).to include("Price can't be blank", "Price Half-width number", "Price Out of setting range")
+        expect(@item.errors.full_messages).to include("Price can't be blank")
       end
 
       it '売価格は、¥300~¥9,999,999の間のみ保存可能であること' do
         @item.price = '100'
         @item.valid?
         expect(@item.errors.full_messages).to include("Price Out of setting range")
+      end
+
+      it '売り価格は、¥9,999,999を超えると出品できない' do
+        @item.price = 10_000_000
+        @item.valid?
+        expect(@item.errors.full_messages).to include('Price must be less than or equal to 9999999')
       end
 
       it '販売価格は半角数字のみ保存可能であること' do
